76554156c9d889393f96f901a47daecfe4d4ccb3
procheta <procheta>
2016-01-28 14:41:09 +0530

    Post comment updated DocVector

--- InferrdAp/src/Evaluator/DocVector.java
+++ InferrdAp/src/Evaluator/DocVector.java
@@ -43,28 +43,11 @@ public class DocVector {
     String docid;
     public ArrayList<TermFreq> termVector;
 
-	/*DG_Comments:
-	 * Don't open the IndexReader everytime while creating a DocVector object. This is highly inefficient
-	 * as IndexReader.open() is an expensive system call.
-	 *
-	 * Don't create the IndexSearcher object everytime. Pass these around from the calling function.
-	 *
-	 */
-
-    public DocVector(String docid, String indexPath) throws IOException, ParseException {
+    public DocVector(String docid, String indexPath, IndexReader reader, IndexSearcher searcher) throws IOException, ParseException {
         this.docid = docid;
-        IndexReader reader = DirectoryReader.open(FSDirectory.open(new File(indexPath)));
-
         Analyzer analyzer = new KeywordAnalyzer();
-        IndexSearcher isearcher = new IndexSearcher(reader);
-		//+++DG_Comments: 
-    	// Use TrecDocParse.FIELD_ID instead of "id"
         QueryParser parser = new QueryParser(Version.LUCENE_CURRENT, "id", analyzer);
-
-        IndexSearcher searcher = new IndexSearcher(reader);
-		//+++DG_Comments: An easier and better way is to directly set the term as a single query term (a BooleanQuery with MUST clause)
-		// than to use QueryParser
-        Query query = parser.parse(docid); 
+        Query query = parser.parse(docid);
         TopDocs topdocs = searcher.search(query, 1);
         int index = topdocs.scoreDocs[0].doc;
 
@@ -83,44 +66,46 @@ public class DocVector {
 
     }
 
-    public double computeCosineSimilarity(DocVector d) {  // DG_Comments: Change name to cosineSim()
+    public double cosineSim(DocVector d) {
         int j = 0;
-		// +++DG_Comments: Give more meaningful names for norm1, norm2 and sum
-        int norm1 = 0;
-        int norm2 = 0;
-        double sum = 0;
-        for (int i = 0; i < termVector.size(); i++) {  // DG_Comments: why check on the bounds of this object only and not on d?
-			// +++DG_Comments: Avoid multiple calls of the compareTo function by calling it only
-			// once and assigning to a variable. You can then check the variable value
-            if (termVector.get(i).term.compareTo(d.termVector.get(j).term) > 0) {
+        int firstsquaredLength = 0;
+        int secondsquaredLength = 0;
+        double freqSum = 0;
+        for (int i = 0; i < termVector.size(); i++) {
+            
+            int compareToValue = termVector.get(i).term.compareTo(d.termVector.get(j).term);
+            if (compareToValue > 0) {
                 if (j < d.termVector.size()) {
                     j++;
-                    norm1 = norm1 + termVector.get(i).freq * termVector.get(i).freq;
-                }                
+                    firstsquaredLength = firstsquaredLength + termVector.get(i).freq * termVector.get(i).freq;
+                }
             }
-            if (termVector.get(i).term.compareTo(d.termVector.get(j).term) < 0) {
+            if (compareToValue< 0) {
                 if (j < d.termVector.size()) {
                     i++;
-                    norm2 = norm2 + d.termVector.get(j).freq * d.termVector.get(j).freq;
-                }                
+                    secondsquaredLength = secondsquaredLength + d.termVector.get(j).freq * d.termVector.get(j).freq;
+                }
             }
-           	//+++DG_Comments: Don't need to check the 0 condition! Simply use else! 
-             if (termVector.get(i).term.compareTo(d.termVector.get(j).term) == 0) {
-                 sum += d.termVector.get(j).freq * termVector.get(i).freq;
-                if (j < d.termVector.size()) 
+
+            if (compareToValue == 0) {
+                freqSum += d.termVector.get(j).freq * termVector.get(i).freq;
+                if (j < d.termVector.size()) {
                     j++;
-                 if(j == d.termVector.size())
-                     break;
-                    norm2 = norm2 + d.termVector.get(j).freq * d.termVector.get(j).freq;
-                    norm1 = norm1 + termVector.get(i).freq * termVector.get(i).freq;
-                                
-            }
-             
-                if(j == d.termVector.size())
+                }
+                if (j == d.termVector.size()) {
                     break;
+                }
+                secondsquaredLength = secondsquaredLength + d.termVector.get(j).freq * d.termVector.get(j).freq;
+                firstsquaredLength = firstsquaredLength + termVector.get(i).freq * termVector.get(i).freq;
+
+            }
+
+            if (j == d.termVector.size()) {
+                break;
+            }
         }
 
-        return sum/(Math.sqrt(norm1) * Math.sqrt(norm2));
+        return freqSum / (Math.sqrt(firstsquaredLength) * Math.sqrt(secondsquaredLength));
     }
 
 }
